bebde0cf800d1004cc088367cfc163ec49816c2e
keisuke <keisuke.seed@gmail.com>
2017-12-21 16:51:53 +0900

    プロジェクト ファイルを追加します。

---
+++ Prism700p5Try/Prism700p5Try/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prism700p5Try.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService) 
+            : base (navigationService)
+        {
+            Title = "Main Page";
+        }
+    }
+}
